bc14db0571b383d2e4a8dd213f33a8aa86089ea3
Anthony Moss <asmoss93@gmail.com>
2019-05-01 15:57:56 -0400

    adds home page at / route

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <span>
+                <h1>🏠</h1>
+            </span>
+        </div>
+    );
+}
+
+export default Home;
