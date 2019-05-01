aa0f2ca3ee2f16f2bd599a4fe695cf9de6adcd3c
Anthony Moss <asmoss93@gmail.com>
2019-05-01 15:57:28 -0400

    adds drink file to handle mapping of drinks array

---
+++ src/Drink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Drink({match}) {
+    return (
+        <div>
+            <h1>{match.params.drink}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit at modi consequatur et, illo similique nostrum rerum voluptatem? Veniam aliquam repellat dolor eius ducimus ex fugit ut laudantium maxime tempore?</p>
+        </div>
+    )
+}
+
+export default Drink
